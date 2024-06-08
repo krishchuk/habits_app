cbefc53a7cb708d5d2058511797d0a6fad815db3
krishchuk <ms.den.best@mail.ru>
2024-06-09 00:12:27 +0500

    add urls, views, serializers and perms for habits

---
+++ habits/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from habits.views import (HabitListAPIView, HabitDetailAPIView, HabitCreateAPIView, HabitUpdateAPIView,
+                          HabitDeleteAPIView)
+
+app_name = "habits"
+
+urlpatterns = [
+    path("habits/", HabitListAPIView.as_view(), name="habit_list"),
+    path("habits/<int:pk>/", HabitDetailAPIView.as_view(), name="habit_detail"),
+    path("habits/create/", HabitCreateAPIView.as_view(), name="habit_create"),
+    path("habits/<int:pk>/update", HabitUpdateAPIView.as_view(), name="habit_update"),
+    path("habits/<int:pk>/delete", HabitDeleteAPIView.as_view(), name="habit_delete"),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import CustomPagination
+from habits.permissions import IsOwner, ReadOnlyUnlessPublic
+from habits.serializers import HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = CustomPagination
+    permission_classes = [IsAuthenticatedOrReadOnly | ReadOnlyUnlessPublic]
+
+
+class HabitDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner | ReadOnlyUnlessPublic]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Habit.objects.filter(user=self.request.user)
+        else:
+            return Habit.objects.filter(is_public=True)
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit.save()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
+
+
+class HabitDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
