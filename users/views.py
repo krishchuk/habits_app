c1fc59a7d4b20be4ef6ed7b879c94d3e9726cc1c
krishchuk <ms.den.best@mail.ru>
2024-06-13 00:58:49 +0500

    flake8

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import UserSerializer
