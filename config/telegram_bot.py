077c13fe1c65db3587d553417f960653e6a6a813
krishchuk <ms.den.best@mail.ru>
2024-06-20 00:03:29 +0500

    add celery-beat

--- config/telegram_bot.py
+++ config/telegram_bot.py
@@ -5,16 +5,6 @@ import requests
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 
-# def send_telegram_message(chat_id, text):
-#     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-#     data = {
-#         "chat_id": chat_id,
-#         "text": text
-#     }
-#     response = requests.post(url, json=data)
-#     return response.json()
-
-
 def send_telegram_message(chat_id, text):
     data = {
         "chat_id": chat_id,
--- habits/tasks.py
+++ habits/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task
 from django.utils import timezone
+import requests
 
-from config.telegram_bot import send_telegram_message
-from .models import Habit
+from config.telegram_bot import send_telegram_message, TELEGRAM_TOKEN
+from habits.models import Habit
 
 
 @shared_task
@@ -15,6 +16,12 @@ def send_habit_notifications():
 @shared_task
 def send_notification(habit_id):
     habit = Habit.objects.get(id=habit_id)
-    chat_id = f'@{habit.user.tg_nick}'
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+    request = requests.get(url).json()
+    print(request)
+    chat_id = request['result'][0]['message']['chat']['id']
+    print(chat_id)
+
     text = f"Не забудьте выполнить привычку: {habit.action}"
     send_telegram_message(chat_id, text)
